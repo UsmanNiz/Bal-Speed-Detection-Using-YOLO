bc1a08f4d555f2fbfa3370416e4be480f8ac803f
Fawad-Javed-Fateh <fawadj.fateh@gmail.com>
2023-05-08 11:16:27 +0500

    Video downloader UI updated to modern UI

--- Youtube_Video_Extractor.py
+++ Youtube_Video_Extractor.py
@@ -1,57 +1,84 @@
 import pytube
 import tkinter as tk
 from tkinter import messagebox
-from tkinter.ttk import Progressbar
 import threading
-import subprocess
+import customtkinter
 
-def download_video(root,video_url):
-    def download():
-        stream.download(filename="videos/yt_vid.mp4")
-        progress_bar.destroy()
-        root.destroy()
+class Extractor_Window(customtkinter.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.geometry("400x250")
+        self.title = "Youtube Video Extractor"
+        self.wm_attributes("")
+        
+        self.label = customtkinter.CTkLabel(self, text="Enter YouTube Video URL:")
+        self.label.pack(padx=5, pady=20)
 
-    def update_progress_bar(stream, chunk, bytes_remaining):
-        total_size = stream.filesize
-        bytes_downloaded = total_size - bytes_remaining
-        progress = bytes_downloaded / total_size * 100
-        progress_bar['value'] = progress
+        self.textbox = customtkinter.CTkEntry(master=self, placeholder_text="Youtube URL", width=360)
+        self.textbox.pack(padx=5)
 
-    url = video_url.get()
-    try:
-        video = pytube.YouTube(url, use_oauth=True, allow_oauth_cache=True, on_progress_callback=update_progress_bar)
-        video.check_availability()
-    except:
-        messagebox.showerror("Error", "Please enter a valid YouTube video URL.")
-        return
+        self.button = customtkinter.CTkButton(master=self, text="Go", command=self.download_video)
+        self.button.pack(padx=5, pady=15)
 
-    stream = video.streams.filter(res="720p").first()
-    if stream is None:
-        stream = video.streams.filter(res="480p").first()
-        if stream is None:
-            stream = video.streams.get_highest_resolution()
-    
-    progress_bar = Progressbar(root, orient=tk.HORIZONTAL, length=200, mode='determinate')
-    progress_bar.pack(pady=20)
+        self.progresstext = customtkinter.CTkLabel(self, text="Download Progress:")
+        self.progressbar = customtkinter.CTkProgressBar(master=self, orientation="horizontal")
+        self.progressbar.set(0)
 
-    threading.Thread.daemon = True
-    threading.Thread(target=download).start()
-    
-    stream.on_progress()
-    progress_bar.start()
+    def download_video(self):
+        def download():
+            stream.download(filename="videos/yt_vid.mp4")
+            self.destroy()
+
+        def update_progress_bar(stream, chunk, bytes_remaining):
+            total_size = stream.filesize
+            bytes_downloaded = total_size - bytes_remaining
+            progress = bytes_downloaded / total_size
+            self.progressbar.set(progress)
+        
+        self.button.configure(state="disabled")
+        url = self.textbox.get()
+        try:
+            video = pytube.YouTube(url, use_oauth=True, allow_oauth_cache=True, on_progress_callback=update_progress_bar)
+            video.check_availability()
+        except:
+            messagebox.showerror("Error", "Please enter a valid YouTube video URL.")
+            self.button.configure(state="normal")
+            return
+
+        try:
+            stream = video.streams.filter(res="720p").first()
+            if stream is None:
+                stream = video.streams.filter(res="480p").first()
+                if stream is None:
+                    stream = video.streams.get_highest_resolution()
+        except:
+            messagebox.showerror("Error", "URL is valid but video is unaccessable from Youtube's side.")
+            self.button.configure(state="normal")
+            return
+
+        self.progresstext.pack(padx=5, pady=10)
+        self.progressbar.pack(padx=5)
+
+        self.attributes('-disabled', True)
+        
+        threading.Thread.daemon = True
+        download_thread = threading.Thread(target=download)
+        download_thread.start()
+
+        stream.on_progress()
 
 
-def run_yt_downloader():
-    root = tk.Tk()
-    root.title("YouTube Video Extractor")
-    root.geometry("400x150")
+# def run_yt_downloader():
+#     root = tk.Tk()
+#     root.title("YouTube Video Extractor")
+#     root.geometry("400x150")
 
-    video_url_label = tk.Label(root, text="Enter YouTube Video URL:")
-    video_url_label.pack(pady=5)
-    video_url = tk.Entry(root, width=50)
-    video_url.pack(pady=5)
-    go_button = tk.Button(root, text="Go", command=lambda:download_video(root,video_url))
-    go_button.pack(pady=5)
-    root.mainloop()
-    print('here')
-    print('here')
+#     video_url_label = tk.Label(root, text="Enter YouTube Video URL:")
+#     video_url_label.pack(pady=5)
+#     video_url = tk.Entry(root, width=50)
+#     video_url.pack(pady=5)
+#     go_button = tk.Button(root, text="Go", command=lambda:download_video(root,video_url))
+#     go_button.pack(pady=5)
+#     root.mainloop()
+#     print('here')
+#     print('here')
--- newui.py
+++ newui.py
@@ -8,7 +8,7 @@ from tkinter import filedialog
 import cv2
 import time
 from detect import *
-from Youtube_Video_Extractor import run_yt_downloader
+from Youtube_Video_Extractor import Extractor_Window
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -74,6 +74,8 @@ class App(customtkinter.CTk):
         
         self.textbox.insert("0.0", "The Diamond Cutter is a unique approach to finding cricket ball speed using modern AI techniques such as Action Recognition and Object Detection. The project runs on pre-recorded videos from your harddrive or directly from a video url from a hosting service. Both modes of functioning are added in this program. This Program runs on a GTX/RTX enabled machine with CUDA runtime and it's relevant pytorch distribution installed.CPU only mode to be added in the future. Before runnning this ensure you have the relevant python version installed and have installed the pre-trained weights.")
 
+        self.toplevel_window = None
+
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
         print("CTkInputDialog:", dialog.get_input())
@@ -88,10 +90,16 @@ class App(customtkinter.CTk):
     def home_button_event(self):
          self.inference_label.grid_remove()
          self.home_page_image_label.grid(row=0, column=1,columnspan=3,padx=(20,20),pady=(20,0))
-         self.textbox.grid(row=2,column=1, columnspan=2,padx=(20, 20),pady=(20,20), sticky="nsew")
+         self.textbox.grid(row=2,column=1, columnspan=2,padx=(20, 20),pady=(20,20), sticky="nsew")        
 
     def URL_button_event(self):
-        run_yt_downloader()
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window = Extractor_Window(self) 
+        else:
+            self.toplevel_window.focus()
+            
+        self.toplevel_window.grab_set()
+        self.wait_window(self.toplevel_window)
         
         self.current_file_path='videos/yt_vid.mp4'
         run_detector(self)
